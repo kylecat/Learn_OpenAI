2040805454bd44e4739bc62d2007e96c4c270ca2
KyleLiuCat <kyle.liu.coffeecat@gmail.com>
2023-03-21 18:51:04 +0800

    Initial commit

---
+++ OpenAI-Whisper.py
@@ -0,0 +1,23 @@
+"""
+Error:
+    SystemError: initialization of _internal failed without raising an exception
+    初始化失敗，Numpy及Numba版本不對打架，解決方案
+    https://github.com/openai/whisper/discussions/1103
+"""
+import whisper
+import pathlib
+
+# 製作檔案路徑（我懶得key）
+PATH = pathlib.Path()
+HOME = PATH.home()
+MP4_ROOT = HOME.joinpath("Desktop/ISHS_Talks")
+MP4_FILE = MP4_ROOT.joinpath("How can CO2 and light co-optimization improve propagation in Vertical Farms Ricardo Hernandez NCSU, USA..mp4")
+
+MP4 = str(MP4_FILE.resolve())
+print(MP4)
+
+model = whisper.load_model("base") # 建立便是模型
+result = model.transcribe(MP4) # 讀取檔案辨識出結果
+
+print(result["text"]) # 把辨識結果的文字印出來
+
---
+++ get_API_Key.py
@@ -0,0 +1,29 @@
+import openai
+
+openai.api_key = "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"
+
+# list models
+models = openai.Model.list()
+
+# print the first model's id
+# models_list = models.data
+# for _model in models_list:
+#     print(_model.id)
+
+# gpt-3.5-turbo
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content":"嗨"}
+    ]
+)
+
+# "choices"=[{回覆狀態(finish_reason), 序號(index), 訊息(message):內容(content), 訊息:角色(role)：助手(assistant)}]
+# "created"
+# "id"
+# "object"
+# "usage" : tokens 使用情況
+
+print("[Bot content]\t", completion.choices[0].message.content)
+print("[Bot role]\t", completion.choices[0].message.role)
+
